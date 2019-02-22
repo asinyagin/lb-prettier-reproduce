461e77d85d16a9e5c4e286ece301558abdf51d51
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2019-02-22 07:37:38 +0000

    Init

---
+++ src/models/user.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    id: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
